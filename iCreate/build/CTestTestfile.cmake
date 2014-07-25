74abc4fc7798ada1200ec526d8bcc6afca938bd7
aarich <arich@hmc.edu>
2014-07-25 16:01:52 +1000

    remove build folder

--- iCreate/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robotics/Desktop/ROS-Controllers/iCreate
-# Build directory: /home/robotics/Desktop/ROS-Controllers/iCreate/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
